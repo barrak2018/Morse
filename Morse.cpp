6aec5bd2d4f4ab6bf58a96d4e0cccfd13c3b039e
barrak2018 <joseestebanojedaaular@gmail.com>
2023-03-14 21:02:34 -0400

    creando dot, dash y la logica de diccionario

--- Morse.cpp
+++ Morse.cpp
@@ -10,8 +10,37 @@ Morse::Morse(int pin, int dotTime){
 
 void Morse::_dot(){
     tone(_pin, 900, _dotDelay);
+    delay(_dotDelay);
+}
+
+void Morse::_dash(){
+    tone(_pin, 900, _dotDelay*3);
+    delay(_dotDelay*3);
 }
 
 void Morse::_dictionary(char letter){
+    switch (letter) {
 
+        
+
+
+
+
+        case '_':
+          // do something
+          break;
+        case '-':
+          // do something
+          break;
+        case '.':
+            _dot();
+            delay(_dotDelay * 3);
+            break;
+        case ' ':
+            _dot();
+            delay((_dotDelay * 3)*3);
+            break;
+        default:
+          // do something
+    }
 }
